18ab256f47cb569bae70876949558624f622df68
Sagar poudel <pdlsgr62@gmail.com>
2024-03-05 08:27:55 +0545

    Learned expres server and environment variable

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-console.log("Testing")
+require('dotenv').config();
+const express = require('express');
+
+
+const app = express();
+
+const port =3000
+
+app.get('/',(req,res)=>{
+
+    res.send('hello worlddddd')
+
+}),
+
+app.get('/test', (req, res)=>{
+
+    res.send('test')
+})
+
+app.get('/login', (req, res)=>{
+    res.send('<h1>Hello sagar poudel.</h1>')
+})
+
+app.listen(process.env.PORT, ()=>{
+    console.log(`Listening on port ${process.env.PORT}`)
+})
